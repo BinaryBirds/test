8b082f1fc7e7ab1405726ba3fcbffbee9b2b9130
Tibor Bodecs <mail.tib@gmail.com>
2023-03-14 18:09:18 +0100

    test failure

--- Tests/testTests/testTests.swift
+++ Tests/testTests/testTests.swift
@@ -8,6 +8,6 @@ final class testTests: XCTestCase {
     }
     
     func testTest222Example() throws {
-        XCTAssertEqual(test().text, "Hello, World!")
+        XCTAssertEqual(test().text, "1Hello, World!")
     }
 }
