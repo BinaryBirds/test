671a909c36b3cc2bb5eccf36182f6fcbb9d3fbb1
Tibor Bödecs <mail.tib@gmail.com>
2023-03-14 20:34:04 +0100

    swift test (#1)
    * connected swift test action
    
    ---------
    
    Co-authored-by: GErP83 <gurrka@gmail.com>

--- Tests/testTests/testTests.swift
+++ Tests/testTests/testTests.swift
@@ -6,4 +6,8 @@ final class testTests: XCTestCase {
     func testExample() throws {
         XCTAssertEqual(test().text, "Hello, World!")
     }
+    
+    func testTest222Example() throws {
+        XCTAssertEqual(test().text, "1Hello, World!")
+    }
 }
